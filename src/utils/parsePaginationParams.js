feaf9425bd9faaeb96b1a213e7cefcec449bcbf6
Viktar Baloshka <vboloshko@gmail.com>
2025-03-10 23:24:45 +0100

    task 3: created pagination

--- src/utils/parsePaginationParams.js
+++ src/utils/parsePaginationParams.js
@@ -1,6 +1,6 @@
 const parseNumber = (number, defaultValue) => {
   const isString = typeof number === 'string';
-  if (isString) return defaultValue;
+  if (!isString) return defaultValue;
 
   const parsedNumber = parseInt(number);
   if (Number.isNaN(parsedNumber)) {
