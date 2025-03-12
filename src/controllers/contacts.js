65b9b8c989fb43221f89f006a5f42d7a0553d702
Viktar Baloshka <vboloshko@gmail.com>
2025-03-12 21:41:49 +0100

    task 4: added function sort

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -7,10 +7,12 @@ import {
 } from '../services/contacts.js';
 import createHttpError from 'http-errors';
 import { parsePaginationParams } from '../utils/parsePaginationParams.js';
+import { parseSortParams } from '../utils/parseSortParams.js';
 
 export const getContactsController = async (req, res) => {
   const { page, perPage } = parsePaginationParams(req.query);
-  const contacts = await getAllContacts({ page, perPage });
+  const { sortOrder, sortBy } = parseSortParams(req.query);
+  const contacts = await getAllContacts({ page, perPage, sortOrder, sortBy });
   res.status(200).json({
     status: 200,
     message: 'Successfully found contacts!',
