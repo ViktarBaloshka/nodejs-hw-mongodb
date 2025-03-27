6fecb23196fcbd5b5d47a0ca7b49c3be0fdc92a3
Viktar Baloshka <vboloshko@gmail.com>
2025-03-27 17:20:50 +0100

    fixed v3

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,4 +1,5 @@
 import Joi from 'joi';
+import { isValidObjectId } from 'mongoose';
 
 export const createContactSchemaValidation = Joi.object({
   name: Joi.string().min(3).max(20).required(),
@@ -11,7 +12,7 @@ export const createContactSchemaValidation = Joi.object({
   contactType: Joi.string().valid('work', 'home', 'personal').required(),
   userId: Joi.string().custom((value, helper) => {
     if (value && !isValidObjectId(value)) {
-      return helper.message('User id should be a valid mongo id');
+      return helper.message("User id should be a valid mongo id");
     }
     return true;
   }),
